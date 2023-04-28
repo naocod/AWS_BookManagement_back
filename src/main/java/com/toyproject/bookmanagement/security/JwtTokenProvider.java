e6bb0431dc0e2bd3f6f1722b4d0befcc8662b008
naocod <8162857@naver.com>
2023-04-28 11:53:26 +0900

    도서 디테일, 추천, 정보 조회 완료

--- src/main/java/com/toyproject/bookmanagement/security/JwtTokenProvider.java
+++ src/main/java/com/toyproject/bookmanagement/security/JwtTokenProvider.java
@@ -72,15 +72,15 @@ public class JwtTokenProvider {
 				
 			return true;
 		} catch (SecurityException | MalformedJwtException e) {
-			log.info("Invalid JWT Token", e);
+//			log.info("Invalid JWT Token", e);
 		} catch (ExpiredJwtException e) {
-			log.info("Expired JWT Token", e);
+//			log.info("Expired JWT Token", e);
 		} catch (UnsupportedJwtException e) {
-			log.info("Unsupported JET Token", e);
+//			log.info("Unsupported JET Token", e);
 		} catch (IllegalArgumentException e) {
-			log.info("IllegalArgument JWT Token", e);
+//			log.info("IllegalArgument JWT Token", e);
 		} catch (Exception e) {
-			log.info("JWT Token Error", e);
+//			log.info("JWT Token Error", e);
 		}
 		
 		return false;
